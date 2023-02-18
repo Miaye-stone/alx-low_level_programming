efd6b7252aac2d2096821630f3de5ba17a08849d
Miaye-stone <omatsonevivian@gmail.com>
2023-02-18 03:54:17 -0800

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 /**
  * main - Entry point of program
  * Return: Always 1 (success)
@@ -7,6 +8,6 @@
 
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
