40cd13ce7499de95296adf22fe839d7460835e7d
Miaye-stone <omatsonevivian@gmail.com>
2023-02-20 21:03:29 -0800

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int i, j;
 
-	for  (i = 0; i <= 9; i++)
+	for  (i = 0; i < 9; i++)
 	{
-		for (j = i; j <= 9; j++)
+		for (j = i; j < 10; j++)
 		{
 			putchar((i % 10) + '0');
 			putchar((j % 10) + '0');
-			
-			if (i == 9 && j == 9)
+
+			if (i == 8 && j == 9)
 			{
 				continue;
 			}
