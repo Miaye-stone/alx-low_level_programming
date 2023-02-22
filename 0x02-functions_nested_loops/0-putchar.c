74beb61aa104e46e7a36c2fef8a73a573fd5d674
Miaye-stone <omatsonevivian@gmail.com>
2023-02-21 21:35:53 -0800

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,15 @@
 
 int main(void)
 {
-	char i[] = "_putchar";
-
-	_putchar(i, '\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
