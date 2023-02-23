517bf3f070eddef6ce0fabfc93eeb3af1c974baa
Miaye-stone <omatsonevivian@gmail.com>
2023-02-22 16:14:27 -0800

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet
+ */
+
+void print_alphabet(void)
+{
+	char sm;
+
+	for (sm = 'a'; sm <= 'z'; sm++)
+	{
+		_putchar(sm);
+	}
+	_putchar('\n');
+}
