1cb691b467bf75eea62fd9f21ba2aa4a00fd98aa
Miaye-stone <omatsonevivian@gmail.com>
2023-02-22 16:26:00 -0800

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10X the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+
+		_putchar('\n');
+	}
+
+}
